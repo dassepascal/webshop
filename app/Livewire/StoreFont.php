f9be815cf7a7a90b0bc4f7e81e87c3ace34e649a
Dasse <pascaldasse56@gmail.com>
2024-11-27 15:21:53 +0100

    page home

---
+++ app/Livewire/StoreFont.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class StoreFont extends Component
+{
+    public function render()
+    {
+        return view('livewire.store-font');
+    }
+}
